e5865e5fa86cfb432ea6af73a7050068a1407817
etienneleba <etienne@lebarillier.fr>
2020-03-19 15:32:03 +0100

    api added

---
+++ src/app/index.js
@@ -0,0 +1,27 @@
+const Blockchain = require('../blockchain');
+const bodyParser = require('body-parser');
+const express = require('express');
+
+
+const HTTP_PORT = process.env.HTTP_PORT || 3001;
+
+const app = express();
+
+app.use(bodyParser.json());
+
+const blockchain = new Blockchain();
+
+app.get('/blocks', (req, res) => {
+    res.json(blockchain.chain)
+});
+
+app.post('/mine', (req, res) => {
+    const block = blockchain.addBlock(req.body.data);
+    console.log(`New block added: ${block.toString()}`);
+
+    res.redirect('blocks');
+})
+
+app.listen(HTTP_PORT, () => {
+    console.log(`listening on port ${HTTP_PORT}`);
+})
